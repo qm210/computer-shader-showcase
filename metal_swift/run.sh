b9d214edd351202c0b10c98013e9ce652997eeb6
qm210 <quantenmace@gmail.com>
2026-04-17 20:57:10 +0200

    folder structure

--- metal_swift/run.sh
+++ metal_swift/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# könnte auch eine einzige .metal-Datei sein, oder drei, ...
 xcrun -sdk macosx metal -c Shaders/Compute.metal -o Compute.air
 xcrun -sdk macosx metal -c Shaders/Render.metal  -o Render.air
 xcrun -sdk macosx metallib Compute.air Render.air -o Shaders.metallib
